afbca718ccd80c13f4e24a4e4e36a96729ce8b28
t-kupp <kupper.thorge@gmail.com>
2024-11-05 11:24:18 +0100

    added html support

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,36 +1,33 @@
+const welcomeTextfield = document.querySelector("#welcome");
+const confirmTextfield = document.querySelector("#confirm");
+const orderBtn = document.querySelector("#orderBtn");
+
 const vegetarian = "Vegetarian Pizza";
 const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
 
 const pizzaPrice = 80;
 
-// Greeting the user
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+// Welcome message
+welcomeTextfield.innerText = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`;
 
-// Get user order
-let orderName;
-while (!checkOrderName(orderName)) {
-  orderName = prompt("Enter the name of the pizza you want to order today.", "Pepperoni Pizza");
-}
-
-let orderQuantity = 0;
-while (orderQuantity < 1 || orderQuantity > 10 || isNaN(orderQuantity)) {
-  orderQuantity = prompt(`How many of ${orderName} do you want? (1-10)`, "1");
-}
-
-// Confirm message
-alert(`Great, I'll get started on your ${orderName}(s) right away, it will cost ${calculateTotal(orderQuantity)}kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`);
+// Order button
+orderBtn.onclick = () => {
+  let orderName = document.querySelector('input[name="pizza"]:checked').value;
+  let orderQuantity = document.querySelector("#quantity").value;
+  confirmTextfield.innerText = `Great, I'll get started on your ${orderName}(s) right away, it will cost ${totalCost(orderQuantity)}kr. The pizzas will take ${cookingTime(orderQuantity)} minutes.`;
+};
 
 // Helper functions
 function checkOrderName(name) {
   return name == vegetarian || name == hawaiian || name == pepperoni;
 }
 
-function calculateTotal(quantity) {
+function totalCost(quantity) {
   return quantity * pizzaPrice;
 }
 
-function calculateCookingTime(quantity) {
+function cookingTime(quantity) {
   if (quantity <= 2) return 10;
   if (quantity <= 5) return 15;
   return 20;
